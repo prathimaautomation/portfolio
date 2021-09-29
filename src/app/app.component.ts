9830f36f64dcf8f6c23410de78d15a8ddd1e84d5
Prathima Joginipelly <75338982+prathimaautomation@users.noreply.github.com>
2021-09-29 13:40:45 +0100

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import {Component, OnInit} from '@angular/core';
+import {environment} from '../environments/environment';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit{
+  title = 'myprofile';
+  public env: any;
+
+  ngOnInit(): void {
+    this.env = environment.env;
+  }
+}
